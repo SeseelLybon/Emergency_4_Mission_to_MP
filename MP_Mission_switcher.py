26540ead3376d8e5f1e2bf5f6341fbb33517e369
Seseel Lybon <Seseellybon@gmail.com>
2020-02-18 14:04:29 +0100

    Moved questions from startup arg to input()

--- MP_Mission_switcher.py
+++ MP_Mission_switcher.py
@@ -3,47 +3,42 @@ import sys
 from shutil import copyfile
 
 
-print("debug", sys.argv)
-loc_game = str(sys.argv[1])
-mission2mp = int(sys.argv[2])
+print("debug", sys.argv);
+#loc_game = str(sys.argv[1]);
+loc_game = ""
+#mission2mp = int(sys.argv[2]);
 
-loc_maps = loc_game+"Data\\Maps\\"
-loc_specs = loc_game+"Data\\Specs\\"
-specs_filename = "fp_params_endless_mp.xml"
-mp_map_filename = "multiplayer"
+loc_maps = loc_game+"Data\\Maps\\";
+loc_specs = loc_game+"Data\\Specs\\";
+specs_filename = "fp_params_endless_mp.xml";
+mp_map_filename = "multiplayer";
 
-map_file_extentions = [".dds", ".e4m", ".eft"]
-
+map_file_extentions = [".dds", ".e4m", ".eft"];
 
 
 # TODO: Let user pick a mission (with names?)
-mission_names = {0:"Restore MP",
-                 1:"Construction Crane Falls On Church",
-                 2:"Fire in Tyre Warehouse",
-                 3:"Undercover Transaction",
-                 4:"Monster Truck accident",
-                 5:"Motorway Brigde Collapses",
-                 6:"Bomb Attack During Summit",
-                 7:"Hacker Causes Power Failure In The City",
-                 8:"Breeched Levee Floods The Town",
-                 9:"Police Raid Downtown",
-                 10:"Explosion In Polar Station",
-                 11:"Oil Rig On Fire",
-                 12:"Train Rams Tourist Coach",
-                 13:"Accident In Mining Facility",
-                 14:"Traffic Accident In Motorway Tunnel",
-                 15:"Explosion At Fireworks Company",
-                 16:"Witness Protection",
-                 17:"Hostage Drama",
-                 18:"Outbreak Of Fire In Outside Concert",
-                 19:"Catastrophic Accident At The Airport",
-                 20:"Great One"
-                 }
-
-if mission2mp == 0:
-	print("Restoring MP")
-else:
-	print("Turning mission", mission2mp, ":",mission_names[mission2mp], "into mp map" )
+mission_names = {0:"0:  Restore MP",
+                 1:"1:  Construction Crane Falls On Church",
+                 2:"2:  Fire in Tyre Warehouse",
+                 3:"3:  Undercover Transaction",
+                 4:"4:  Monster Truck accident",
+                 5:"5:  Motorway Brigde Collapses",
+                 6:"6:  Bomb Attack During Summit",
+                 7:"7:  Hacker Causes Power Failure In The City",
+                 8:"8:  Breeched Levee Floods The Town",
+                 9:"9:  Police Raid Downtown",
+                 10:"10: Explosion In Polar Station",
+                 11:"11: Oil Rig On Fire",
+                 12:"12: Train Rams Tourist Coach",
+                 13:"13: Accident In Mining Facility",
+                 14:"14: Traffic Accident In Motorway Tunnel",
+                 15:"15: Explosion At Fireworks Company",
+                 16:"16: Witness Protection",
+                 17:"17: Hostage Drama",
+                 18:"18: Outbreak Of Fire In Outside Concert",
+                 19:"19: Catastrophic Accident At The Airport",
+                 20:"20: The Great One"
+                 };
 
 mission_file_names = {0:"multiplayer.b",
                       1:"m01",
@@ -66,32 +61,47 @@ mission_file_names = {0:"multiplayer.b",
                       18:"m18",
                       19:"m19",
                       20:"m20"
-                      }
+                      };
+
+print("Which mission to use for MP?:",end="\n");
+for k,v in mission_names.items():
+    print(v);
+userinput = input();
+mission2mp = int(userinput);
 
 
+if mission2mp == 0:
+    print("Restoring MP");
+else:
+    print("Turning mission",mission_names[mission2mp], "into mp map" );
+
+    # TODO: Backup the MP mission files
+    print("Making backup of mp mission files if there is none");
+
+    for i in range(3):
+        try:
+            copyfile(loc_maps + mp_map_filename + map_file_extentions[i],
+                     loc_maps + mp_map_filename + ".b" + map_file_extentions[i]);
+            print("backing up  " + loc_maps + mp_map_filename + map_file_extentions[i]);
+        except FileNotFoundError:
+            print("There is no multiplayer." + map_file_extentions[i] + " file to copy.");
+        except FileExistsError:
+            print("A backup of multiplayer." + map_file_extentions[i] + " already exists.");
 
 
-
-# TODO: Backup the MP mission files
-for i in range(3):
-    try:
-        copyfile(loc_maps+mp_map_filename+map_file_extentions[i], loc_maps+mp_map_filename+".b"+map_file_extentions[i] )
-    except FileNotFoundError:
-        print("There is no multiplayer.x file to copy.")
-    except FileExistsError:
-        print("A backup of multiplayer.x already exists.")
 
 
 # TODO: Copy and remane the mission files to MP mission files.
-
+print("Copy and renaming ");
 for i in range(3):
-    copyfile(loc_maps+mission_file_names[mission2mp]+map_file_extentions[i],loc_maps+mp_map_filename+map_file_extentions[i])
+    copyfile(loc_maps+mission_file_names[mission2mp]+map_file_extentions[i],loc_maps+mp_map_filename+map_file_extentions[i]);
+
 
 # TODO: Replace the mission spec file with one that doesn't cause events (or an alternate file?)
 
+
 # TODO: Tell the user he's ready
-print("Done?")
+print("Done?");
+
 
 # TODO: keep program running to put files back manually when done? Alternate option to reset files?
-
-# TODO: Create github repo
